75d01f83d9955064b04e8b3e3db1d43e212459a6
小新 <782446723@qq.com>
2023-07-04 11:22:56 +0800

    可以展示跳马过程中的路径

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ const int BOARD_SIZE = 200;
 struct Position {
     int x;
     int y;
+    Position() : x(0), y(0) {}
     Position(int _x, int _y) : x(_x), y(_y) {}
 };
 
@@ -23,10 +24,11 @@ bool isValid(int x, int y) {
     return (x >= 1 && x <= BOARD_SIZE && y >= 1 && y <= BOARD_SIZE);
 }
 
-// 计算马从当前位置到目标位置的最少跳数
-int minJumps(Position start, Position target) {
+// 计算马从当前位置到目标位置的最少跳数，并返回路径
+Vector<Position> minJumps(Position start, Position target) {
     // 初始化棋盘和跳数
     Vector<Vector<int>> board(BOARD_SIZE + 1, Vector<int>(BOARD_SIZE + 1, -1));
+    Vector<Vector<Position>> parent(BOARD_SIZE + 1, Vector<Position>(BOARD_SIZE + 1, {-1, -1}));
     board[start.x][start.y] = 0;
 
     // 使用广度优先搜索来计算最少跳数
@@ -38,8 +40,14 @@ int minJumps(Position start, Position target) {
         q.pop();
 
         if (curr.x == target.x && curr.y == target.y) {
-            // 找到目标位置
-            return board[curr.x][curr.y];
+            // 找到目标位置，构建路径
+            Vector<Position> path;
+            while (!(curr.x == start.x && curr.y == start.y)) {
+                path.push_back(curr);
+                curr = parent[curr.x][curr.y];
+            }
+            path.push_back(start);
+            return path;
         }
 
         // 尝试马的八个方向的移动
@@ -49,13 +57,14 @@ int minJumps(Position start, Position target) {
 
             if (isValid(nextX, nextY) && board[nextX][nextY] == -1) {
                 board[nextX][nextY] = board[curr.x][curr.y] + 1;
+                parent[nextX][nextY] = curr;
                 q.push(Position(nextX, nextY));
             }
         }
     }
 
-    // 没有找到目标位置
-    return -1;
+    // 没有找到目标位置，返回空路径
+    return Vector<Position>();
 }
 
 int main() {
@@ -65,8 +74,20 @@ int main() {
     Position start(startX, startY);
     Position target(targetX, targetY);
 
-    int minJumpsRequired = minJumps(start, target);
-    cout << minJumpsRequired << endl;
+    Vector<Position> path = minJumps(start, target);
+    if (path.empty()) {
+        cout << "No path found." << endl;
+    } else {
+        cout << "Minimum jumps required: " << path.size() - 1 << endl;
+        cout << "Path: ";
+        for (int i = path.size() - 1; i >= 0; i--) {
+            cout << "(" << path[i].x << ", " << path[i].y << ")";
+            if (i != 0) {
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
