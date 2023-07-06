73a94ad42df13e066e8e8fe805bf01a3ed5a30a4
小新 <782446723@qq.com>
2023-07-06 08:29:26 +0800

    转换为数据结构的写法

--- main.cpp
+++ main.cpp
@@ -7,97 +7,96 @@
 using namespace std;
 using json = nlohmann::json;
 
-// 定义马的移动方向
-const int dx[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
-const int dy[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+class Chessboard {
+public:
+    struct Position {
+        int x;
+        int y;
+        Position() : x(0), y(0) {}
+        Position(int _x, int _y) : x(_x), y(_y) {}
+    };
 
-// 定义棋盘大小
-const int BOARD_SIZE = 200;
+private:
+    const int boardSize = 200;
+    const int dx[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+    const int dy[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
 
-// 定义棋盘坐标结构体
-struct Position {
-	int x;
-	int y;
-	Position() : x(0), y(0) {}
-	Position(int _x, int _y) : x(_x), y(_y) {}
+public:
+    bool isValid(int x, int y) {
+        return (x >= 1 && x <= boardSize && y >= 1 && y <= boardSize);
+    }
+
+    Vector<Position> minJumps(Position start, Position target) {
+        Vector<Vector<int>> board(boardSize + 1, Vector<int>(boardSize + 1, -1));
+        Vector<Vector<Position>> parent(boardSize + 1, Vector<Position>(boardSize + 1, { -1, -1 }));
+        board[start.x][start.y] = 0;
+
+        Queue<Position> q;
+        q.push(start);
+
+        while (!q.empty()) {
+            Position curr = q.front();
+            q.pop();
+
+            if (curr.x == target.x && curr.y == target.y) {
+                Vector<Position> path;
+                while (!(curr.x == start.x && curr.y == start.y)) {
+                    path.push_back(curr);
+                    curr = parent[curr.x][curr.y];
+                }
+                path.push_back(start);
+                return path;
+            }
+
+            for (int i = 0; i < 8; i++) {
+                int nextX = curr.x + dx[i];
+                int nextY = curr.y + dy[i];
+
+                if (isValid(nextX, nextY) && board[nextX][nextY] == -1) {
+                    board[nextX][nextY] = board[curr.x][curr.y] + 1;
+                    parent[nextX][nextY] = curr;
+                    q.push(Position(nextX, nextY));
+                }
+            }
+        }
+
+        return Vector<Position>();
+    }
+
+    void solve() {
+        int startX, startY, targetX, targetY;
+        cin >> startX >> startY >> targetX >> targetY;
+
+        Position start(startX, startY);
+        Position target(targetX, targetY);
+
+        Vector<Position> path = minJumps(start, target);
+        if (path.empty()) {
+            cout << "没有找到路径." << endl;
+        } else {
+            cout << "最少跳数: " << path.size() - 1 << endl;
+            cout << "路径: ";
+            for (int i = path.size() - 1; i >= 0; i--) {
+                cout << "(" << path[i].x << ", " << path[i].y << ")";
+                if (i != 0) {
+                    cout << " -> ";
+                }
+            }
+            cout << endl;
+
+            json json_path;
+            for (const auto& pos : path) {
+                json_path.push_back({ {"x", pos.x}, {"y", pos.y} });
+            }
+            json result = { {"minJumps", path.size() - 1}, {"start", {{"x", start.x}, {"y", start.y}}}, {"target", {{"x", target.x}, {"y", target.y}}}, {"path", json_path} };
+            cout << result.dump() << endl;
+        }
+    }
 };
 
-// 检查坐标是否在棋盘范围内
-bool isValid(int x, int y) {
-	return (x >= 1 && x <= BOARD_SIZE && y >= 1 && y <= BOARD_SIZE);
-}
-
-// 使用广度优先搜索来计算最少跳数
-Vector<Position> minJumps(Position start, Position target) {
-	// 初始化棋盘和跳数
-	Vector<Vector<int>> board(BOARD_SIZE + 1, Vector<int>(BOARD_SIZE + 1, -1));
-	Vector<Vector<Position>> parent(BOARD_SIZE + 1, Vector<Position>(BOARD_SIZE + 1, { -1, -1 }));
-	board[start.x][start.y] = 0;
-
-	Queue<Position> q;
-	q.push(start);
-
-	while (!q.empty()) {
-		Position curr = q.front();
-		q.pop();
-
-		if (curr.x == target.x && curr.y == target.y) {
-			// 找到目标位置，构建路径
-			Vector<Position> path;
-			while (!(curr.x == start.x && curr.y == start.y)) {
-				path.push_back(curr);
-				curr = parent[curr.x][curr.y];
-			}
-			path.push_back(start);
-			return path;
-		}
-
-		// 尝试马的八个方向的移动
-		for (int i = 0; i < 8; i++) {
-			int nextX = curr.x + dx[i];
-			int nextY = curr.y + dy[i];
-
-			if (isValid(nextX, nextY) && board[nextX][nextY] == -1) {
-				board[nextX][nextY] = board[curr.x][curr.y] + 1;
-				parent[nextX][nextY] = curr;
-				q.push(Position(nextX, nextY));
-			}
-		}
-	}
-
-	// 没有找到目标位置，返回空路径
-	return Vector<Position>();
-}
-
 int main() {
-	int startX, startY, targetX, targetY;
-	cin >> startX >> startY >> targetX >> targetY;
+    Chessboard chessboard;
+    chessboard.solve();
 
-	Position start(startX, startY);
-	Position target(targetX, targetY);
-
-	Vector<Position> path = minJumps(start, target);
-	if (path.empty()) {
-		cout << "没有找到路径." << endl;
-	} else {
-		cout << "最少跳数: " << path.size() - 1 << endl;
-		cout << "路径: ";
-		for (int i = path.size() - 1; i >= 0; i--) {
-			cout << "(" << path[i].x << ", " << path[i].y << ")";
-			if (i != 0) {
-				cout << " -> ";
-			}
-		}
-		cout << endl;
-
-		// 输出 json 格式的路径
-		json json_path;
-		for (const auto& pos : path) {
-			json_path.push_back({ {"x", pos.x}, {"y", pos.y} });
-		}
-		json result = { {"minJumps", path.size() - 1}, {"start", {{"x", start.x}, {"y", start.y}}}, {"target", {{"x", target.x}, {"y", target.y}}}, {"path", json_path} };
-		cout << result.dump() << endl;
-	}
-
-	return 0;
+    return 0;
 }
